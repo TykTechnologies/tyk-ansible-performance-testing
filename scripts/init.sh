ec92f5e3b1f4e8ac0057610b428911c3db46a3b7
Zaid Albirawi <zaid@tyk.io>
2022-03-14 15:55:10 -0400

    Copy aws and gcp confing in init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,4 +2,6 @@ cp ansible.cfg.example ansible.cfg
 cp hosts.yml.example hosts.yml
 cp vars/tests.rest.yml.example vars/tests.yml
 cp vars/services.yml.example vars/services.yml
+cp vars/aws.yml.example vars/aws.yml
+cp vars/gcp.yml.example vars/gcp.yml
 ansible-galaxy install -r requirements.yml
