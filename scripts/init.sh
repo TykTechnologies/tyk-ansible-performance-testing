7290b621dd1a5cc8e1f42bddec33c11fd6e44380
Zaid Albirawi <zaid@tyk.io>
2022-03-17 10:24:17 -0400

    Add saas support

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,6 @@
 cp ansible.cfg.example ansible.cfg
 cp hosts.yml.example hosts.yml
+cp saas/hosts.yml.example saas/hosts.yml
 cp vars/tests.rest.yml.example vars/tests.yml
 cp vars/services.yml.example vars/services.yml
 cp vars/aws.yml.example vars/aws.yml
