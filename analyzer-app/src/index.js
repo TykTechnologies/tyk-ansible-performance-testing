11d82afb18640ec55db3dac9ec2188f5618273e1
Zaid Albirawi <zaid@tyk.io>
2022-09-07 20:39:16 -0400

    Update fonts

--- analyzer-app/src/index.js
+++ analyzer-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import './fonts/Smoolthan/Smoolthan Bold.otf'
+import './fonts/Smoolthan/Smoolthan_Bold.otf'
 import './index.css'
 
 const analyzer = document.getElementById('analyzer'),
